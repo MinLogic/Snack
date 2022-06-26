d6363f5d450868b8daca639791acc54a47263791
MinLogic <dobjmin995@gmail.com>
2022-06-27 03:02:58 +0900

    content 처리하기

--- src/main/java/com/dataworld/server/RequestHandler.java
+++ src/main/java/com/dataworld/server/RequestHandler.java
@@ -9,6 +9,7 @@ import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.Socket;
 import java.net.URL;
+import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.file.Files;
 
@@ -43,6 +44,8 @@ public class RequestHandler extends Thread{
                 log.debug("header : {}", line);
             }
 
+
+
             if("/product/regForm".equals(tokens[1])){
                 // 상품 폼 페이지
                 DataOutputStream dos = new DataOutputStream(out);
@@ -53,7 +56,18 @@ public class RequestHandler extends Thread{
 
             if("/product".equals(tokens[1])){
                 // 상품 등록
+                String content = br.readLine();
+                String[] contentToken = content.split("&");
+
+                String goodsName = contentToken[0].split("=")[1];
+                int goodsPrice = Integer.parseInt(contentToken[1].split("=")[1]);
                 GoodsList goodsList = GoodsList.getInstance();
+                goodsList.addGoods(new Goods(goodsName, goodsPrice));
+
+                Goods goods = goodsList.retrieveGoods(goodsName);
+                log.debug("success");
+                log.debug("goodsName : {}", goods.getGoodsName());
+                log.debug("goodsPrice : {}", goods.getPrice());
             }
 
 
